8f6248c130883548001b4bb90a0e0d393510d6e6
Pilen <fiskomaten@gmail.com>
2013-04-21 19:01:26 +0200

    Lad os få nogle filer op

---
+++ config-example.py
@@ -0,0 +1,6 @@
+CSRF_ENABLED = True
+DATABASE = 'example.db'
+DEBUG = False
+SECRET_KEY = 'randomly generated secret development key'
+USERNAME = 'admin'
+PASSWORD = 'hunter2'
---
+++ populate_test.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO Ture(tur_navn) VALUES('DiFrost');
+INSERT INTO Ture(tur_navn) VALUES('Reboot');
+
+INSERT INTO Russer(navn, opringet) VALUES("Søren", 0);
+INSERT INTO Russer(navn, opringet) VALUES("Caro", 0);
+INSERT INTO Russer(navn, opringet) VALUES("Mikkel", 0);
+INSERT INTO Russer(navn, opringet) VALUES("Troels", 0);
