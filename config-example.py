f359c548af8f10a99facb3282af2e1b3bdc8dffb
Pilen <fiskomaten@gmail.com>
2013-06-14 23:31:20 +0200

    updated config example

--- config-example.py
+++ config-example.py
@@ -2,5 +2,4 @@ CSRF_ENABLED = True
 DATABASE = 'example.db'
 DEBUG = False
 SECRET_KEY = 'randomly generated secret development key'
-USERNAME = 'admin'
-PASSWORD = 'hunter2'
+BCRYPT_LOG_ROUNDS = 12
