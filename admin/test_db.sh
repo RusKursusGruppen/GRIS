f1882f3cda2e740d101461c3a37d9ab5ff2d6a88
Pilen <fiskomaten@gmail.com>
2013-07-24 23:41:51 +0200

    gave the test users names

--- admin/test_db.sh
+++ admin/test_db.sh
@@ -2,7 +2,7 @@
 sqlite3 data.db < admin/schema.sql
 python2 -c "from applications import usermanager;
 usermanager.create_user('rkg','abe','RKG',1)
-usermanager.create_user('fugl','123');
-usermanager.create_user('kat','123');
-usermanager.create_user('tiger','123');
+usermanager.create_user('fugl','123', 'FUGL');
+usermanager.create_user('kat','123', 'KAT');
+usermanager.create_user('tiger','123', 'TIGER');
 "
