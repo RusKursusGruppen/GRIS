c8dc8f06720019f199d9eed44fb033af5a2ce4ce
Pilen <fiskomaten@gmail.com>
2013-08-05 13:03:10 +0200

    updated config example

--- config-example.py
+++ config-example.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 CSRF_ENABLED = True
-DATABASE = 'example.db'
-DEBUG = False
+DATABASE = 'data.db'
+DEBUG = True
 SECRET_KEY = 'randomly generated secret development key'
 BCRYPT_LOG_ROUNDS = 12
 USER_CREATION_KEY_MIN_LENGTH = 128
 USER_CREATION_KEY_MAX_LENGTH = 256
 EMAIL = "name <mail@something.com>"
+LOGFILE = "log.txt"
+PRINT_LOG = False
