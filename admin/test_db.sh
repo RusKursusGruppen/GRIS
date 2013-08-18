6ce764442a5be5911db7ab5700619a4829813799
Pilen <fiskomaten@gmail.com>
2013-08-18 19:19:53 +0200

    rkg is now in all groups

--- admin/test_db.sh
+++ admin/test_db.sh
@@ -1,7 +1,7 @@
 
 sqlite3 data.db < admin/schema.sql
 python2 -c "from applications import usermanager;
-usermanager.create_user('rkg','abe','RKG',['admin'])
+usermanager.create_user('rkg','abe','RKG',['admin', 'rkg', 'tutor', 'mentor'])
 usermanager.create_user('fugl','123', 'FUGL');
 usermanager.create_user('kat','123', 'KAT');
 usermanager.create_user('tiger','123', 'TIGER');
