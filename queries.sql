1d24751fa12acfbdf70ca410fa6f6dc41d0715c9
Pilen <fiskomaten@gmail.com>
2014-04-14 01:44:43 +0200

    the calculations in queries.sql are now correct, we still need to implement the payments

--- queries.sql
+++ queries.sql
@@ -20,6 +20,7 @@ SELECT e_id, SUM(share) AS share_total FROM Debts GROUP BY e_id;
 -- Entries with share_totals per book
 SELECT * FROM Entries LEFT OUTER JOIN (SELECT e_id, SUM(share) AS share_total FROM Debts GROUP BY e_id) USING (e_id) WHERE b_id = "1";
 
+
 -- debts with entry information
 SELECT * FROM debts LEFT OUTER JOIN entries USING(e_id);
 
@@ -33,6 +34,8 @@ SELECT *, ((amount*1.0)/share_total*share) AS debt FROM debts LEFT OUTER JOIN en
 -- total debt
 SELECT debtor AS user, SUM(debt) AS debt FROM (SELECT *, ((amount*1.0)/share_total*share) AS debt FROM debts LEFT OUTER JOIN entries USING(e_id) LEFT OUTER JOIN (SELECT e_id, SUM(share) AS share_total FROM Debts GROUP BY e_id) USING(e_id) WHERE b_id = "1") GROUP BY debtor;
 
+-- local total debt to you
+select *, sum(debt) as debt from (SELECT *, ((amount*1.0)/share_total*share) AS debt FROM debts LEFT OUTER JOIN entries USING(e_id) LEFT OUTER JOIN (SELECT e_id, SUM(share) AS share_total FROM Debts GROUP BY e_id) USING(e_id) WHERE b_id = "1") where creditor == "rkg" group by debtor;
 
 
 
@@ -41,3 +44,47 @@ SELECT * FROM (SELECT * FROM (SELECT creditor AS user FROM entries WHERE b_id = 
 
 -- breakdown returning float (* 1.0 instead of * 1)
 SELECT *, (IFNULL(credit, 0)-IFNULL(debt,0)) AS balance FROM (SELECT * FROM (SELECT creditor AS user FROM entries WHERE b_id = "1") UNION SELECT debtor AS user FROM debts LEFT OUTER JOIN entries USING(e_id) WHERE b_id = "1" UNION SELECT participant AS user FROM Book_participants WHERE b_id = "1")    LEFT OUTER JOIN    (SELECT creditor AS user, SUM(amount) AS credit FROM entries WHERE b_id = "1" GROUP BY creditor) USING (user)    LEFT OUTER JOIN    (SELECT debtor AS user, SUM(debt) AS debt FROM (SELECT *, ((amount*1.0)/share_total*share) AS debt FROM debts LEFT OUTER JOIN entries USING(e_id) LEFT OUTER JOIN (SELECT e_id, SUM(share) AS share_total FROM Debts GROUP BY e_id) USING(e_id) WHERE b_id = "1") GROUP BY debtor) USING(user);
+
+
+
+
+
+--- local_totals_sql = """
+SELECT *
+FROM
+  (SELECT creditor,
+          SUM(owes) AS total
+   FROM
+
+     /* raw_entries_sql: */
+     (SELECT *,
+      ((amount*1)/share_total*share) AS owes
+      FROM
+        (SELECT *
+         FROM Entries
+         WHERE b_id = "1")
+      LEFT OUTER JOIN
+        (SELECT e_id,
+                SUM(share) AS share_total
+         FROM Debts
+         GROUP BY e_id)
+        USING (e_id)
+      LEFT OUTER JOIN
+        (SELECT e_id,
+                share
+         FROM Debts
+         WHERE debtor = "rkg")
+        USING(e_id))
+
+   GROUP BY creditor)
+WHERE total is not Null AND creditor != "rkg"
+--- """
+
+
+
+
+select *, sum(debt) as debt from (SELECT *, ((amount*1.0)/share_total*share) AS debt FROM debts LEFT OUTER JOIN entries USING(e_id) LEFT OUTER JOIN (SELECT e_id, SUM(share) AS share_total FROM Debts GROUP BY e_id) USING(e_id) WHERE b_id = "1") where creditor == "rkg" group by debtor;
+
+-- calculate who you owes money to -- local_totals
+-- subtract
+-- calculate who owes money to you
