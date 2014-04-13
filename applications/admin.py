1ddc9ad726f424ab4c1e9812beba0df9e14a1b3e
Rotte <mariacarolinemiller@gmail.com>
2014-04-13 18:39:29 +0200

    Possible to delete users by setting the deleted-flag on users

--- applications/admin.py
+++ applications/admin.py
@@ -4,7 +4,7 @@ import datetime, string, time, subprocess
 
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, get_flashed_messages, escape, Blueprint
 
-from lib import data, password, mail
+from lib import data, password, mail, html
 from lib.tools import logged_in, empty, url_front
 
 import config
@@ -29,3 +29,35 @@ def git_pull():
     else:
         response = ""
     return render_template("admin/git_pull.html",response=response)
+
+@admin.route('/admin/delete_user', methods=["GET", "POST"])
+@logged_in('admin')
+def delete_user():
+#    return render_template("admin/delete_user.html")
+    if request.method == "POST":
+        if 'cancel' in request.form:
+            flash(escape("Ændringer anulleret"))
+            return redirect(url_for('admin.overview'))
+        
+        b = data.Bucket(request.form)
+        b.deleted = 1
+        b >> ("UPDATE Users SET $ WHERE username = ?", request.form["user"])
+
+        flash("Bruger slettet")
+
+        return redirect(url_for('admin.delete_user'))
+    
+    else:
+        users = data.execute("SELECT * FROM Users WHERE deleted == 0")
+        users = [(user['username'], "{0}: {1}".format(user['username'], user['name'])) for user in users]
+    
+        w = html.WebBuilder()
+        w.form()
+        w.formtable()
+        w.select("user", "Brugere:", users)
+        form = w.create()
+
+        return render_template("admin/delete_user.html", form=form)
+    
+
+    
