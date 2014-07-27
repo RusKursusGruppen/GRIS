53b916cfb4e5dc5c65d22d5c44301d393743c713
Pilen <fiskomaten@gmail.com>
2014-07-27 15:28:12 +0200

    changed configuration file format

--- config-example.py
+++ config-example.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 CSRF_ENABLED = True
-DATABASE = 'data.db'
 DEBUG = True
 SECRET_KEY = 'randomly generated secret development key'
 BCRYPT_LOG_ROUNDS = 12
@@ -10,3 +9,9 @@ USER_CREATION_KEY_MAX_LENGTH = 256
 EMAIL = "name <mail@something.com>"
 LOGFILE = "log.txt"
 PRINT_LOG = False
+
+DATABASE_HOST = ""
+DATABASE_NAME = ""
+DATABASE_USER = ""
+DATABASE_PORT = 5432
+DATABASE_PASSWORD = ""
