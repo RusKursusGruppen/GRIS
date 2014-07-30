0cb095f2fd0fa83749a08b7ae2e7f1f55efa6fbb
Pilen <fiskomaten@gmail.com>
2014-07-30 16:31:28 +0200

    added password decoding for handling utf-8

--- lib/password.py
+++ lib/password.py
@@ -5,7 +5,7 @@ import bcrypt
 
 def encode(raw_password):
     salt = bcrypt.gensalt(config.BCRYPT_LOG_ROUNDS)
-    return bcrypt.hashpw(raw_password.encode('utf-8'), salt)
+    return bcrypt.hashpw(raw_password.encode('utf-8'), salt).decode('utf-8')
 
 def check(raw_password, enc_password):
     return bcrypt.hashpw(raw_password.encode('utf-8'), enc_password.encode('utf-8')) == enc_password.encode('utf-8')
