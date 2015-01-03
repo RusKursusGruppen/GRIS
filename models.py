4d80ed2eea41acc8cd12d3066529e8109d63246b
Pilen <fiskomaten@gmail.com>
2015-01-03 21:51:52 +0100

    added Groups to models

--- models.py
+++ models.py
@@ -2,12 +2,18 @@
 
 from gris import db
 Model = db.Model
+Table = db.Table
+
 Column = db.Column
 Integer = db.Integer
 Text = db.Text # Use this instead of strings
 Boolean = db.Boolean
 Date = db.Date
+ForeignKey = db.ForeignKey
+relationship = db.relationship
+backref = db.backref
 
+### USERS ###
 class User(db.Model):
     id = Column(Integer, primary_key=True)
     username = Column(Text, unique=True, nullable=False)
@@ -26,3 +32,32 @@ class User(db.Model):
     about_me = Column(Text)
 
     deleted = Column(Boolean, default=False) # Field for marking a user as deleted
+
+    # def __init__(self, username, raw_password, name, email, groups=[]):
+    #     """Create a new user."""
+    #     self.username = username
+    #     self.password = raw_password # TODO: fix!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+    #     self.name = name
+    #     self.email = email
+
+    #     added_groups = []
+    #     for group in groups:
+    #         if isinstance(group, Group):
+    #             self.groups.append(group)
+    #         else:
+    #             groupname = group
+    #             group = Group.query.filter_by(groupname=groupname).first()
+    #             if group is None:
+    #                 group = Group(groupname)
+    #                 added_groups.append(group)
+
+class Group(Model):
+    id = Column(Integer, primary_key=True)
+    groupname = Column(Text, unique=True, nullable=False)
+    def __init__(self, groupname):
+        self.groupname = groupname
+
+users_groups = Table("users_groups",
+                     Column("user_id", Integer, ForeignKey("user.id")),
+                     Column("group_id", Integer, ForeignKey("group.id")))
+User.groups = relationship("Group", secondary=users_groups, backref="users")
