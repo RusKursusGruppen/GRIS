1673eab1516410b8b927b61e7d77b2013802ca87
Pilen <fiskomaten@gmail.com>
2015-01-03 21:53:08 +0100

    the 4 groups admin, admin_mail_log, rkg and mentor will automatically be added on database creation

--- admin/db_create.py
+++ admin/db_create.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from gris import db
+from models import Group
 
 def db_create():
     print("Creating database")
     db.create_all()
+
+    db.session.add_all([
+        Group("admin"),
+        Group("admin_mail_log"),
+        Group("rkg"),
+        Group("mentor")])
+
+    db.session.commit()
 
 if __name__ == "__main__":
     db_create()
