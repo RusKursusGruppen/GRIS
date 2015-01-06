75125d5840b59b5f83ad15dd10ad156b0c8a358d
Pilen <fiskomaten@gmail.com>
2015-01-06 22:22:33 +0100

    added data library, updated from gris1 with transactions

---
+++ lib/data.py
@@ -0,0 +1,241 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import sys
+from contextlib import contextmanager
+import psycopg2
+import psycopg2.extras
+import itertools
+
+from lib import log
+
+import config
+
+class Transaction():
+    def __init__(self):
+        self.connection = psycopg2.connect(host=config.DATABASE_HOST,
+                                           database=config.DATABASE_NAME,
+                                           user=config.DATABASE_USER,
+                                           port=config.DATABASE_PORT,
+                                           password=config.DATABASE_PASSWORD)
+
+    def _execute(self, query, args, many):
+        if query.count("?") != len(args):
+            raise Exception("Wrong number of SQL arguments for query "+query)
+        query = query.replace("?", "%s")
+        try:
+            with self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+                if many:
+                    cursor.executemany(query, args)
+                else:
+                    cursor.execute(query, args)
+
+                log.data(query, args)
+                try:
+                    result = QueryList(cursor.fetchall())
+                    result.rowcount = cursor.rowcount
+                    result.lastrowid = cursor.lastrowid
+                    result.statusmessage = cursor.statusmessage
+                    return result
+                except psycopg2.ProgrammingError as e:
+                    if str(e) == "no results to fetch":
+                        return None
+                    raise
+        except:
+            log.data(query, args, error=True)
+            raise
+
+    def execute(self, query, *args):
+        return self._execute(query, args, False)
+
+    def executemany(self, query, argSeq):
+        args = []
+        query_count = query.count("?")
+        for arg in argSeq:
+            # if argSeq is like ["a", "b", "c", "d", ...]
+            if isinstance(arg, str):
+                length = 1
+                args.append((arg,))
+            else:
+                # if argSeq is like [("a", "b"), ("c", "d"), ...]
+                try:
+                    length = len(arg)
+                    args.append(arg)
+                # if argSeq is like [1, 2, 3, 4...] where the arguments are not strings
+                except:
+                    length = 1
+                    args.append((arg,))
+            if query_count != length:
+                raise Exception("Wrong number of SQL arguments for query "+query)
+
+        query = query.replace("?", "%s")
+        return self._execute(query, args, True)
+
+    def script(self, filename):
+        with open(filename) as f:
+            return self._execute(f.read())
+
+    def commit(self):
+        self.connection.commit()
+
+    def rollback(self):
+        self.connection.rollback()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.commit()
+
+class QueryList(list):
+    def all(self):
+        return self
+
+    def one(self):
+        if len(self) != 1:
+            raise Exception("There are {} rows in the result, but only one was expected!".format(len(self)))
+        else:
+            return self[0]
+
+    def scalar(self):
+        if len(self) != 1:
+            raise Exception("There are {} rows in the result, but only one was expected!".format(len(self)))
+        row = self[0]
+        if len(row) != 1:
+            raise Exception("There are {} columns in the result, but only one was expected!".format(len(self)))
+        return row[0]
+
+def execute(query, *args):
+    with Transaction() as t:
+        return t.execute(query, *args)
+
+def executemany(query, argSeq):
+    with Transaction() as t:
+        return t.executemany(query, *args)
+
+def script(filename):
+    with Transaction() as t:
+        return t.script(filename)
+
+def store(bucket, sql, *args):
+    bucket >> [sql]+list(args)
+
+class Bucket(object):
+    def __init__(self, *unsafe,  **kwargs):
+        self.__lock__ = False
+
+        self.__unsafe__ = {}
+        for d in unsafe:
+            # EXPLANATION:
+            # You cant use self.__unsafe__.update(d) here as the request.form
+            # for some reason will pack its values in lists
+            for k,v in d.items():
+                self.__unsafe__[k] = v
+
+        for k,v in kwargs.items():
+            object.__setattr__(self, k, v)
+
+    def __getattribute__(self, item):
+        """If there is an attribute 'item' in self, return it.
+        If there is a key 'item' in __kv__ return the corresponding value, and insert it into the attributes.
+        Else try to look up 'item' in self anyway probably triggering an exception"""
+
+        if item in object.__getattribute__(self, "__dict__"):
+            return object.__getattribute__(self, item)
+
+        if not object.__getattribute__(self, "__lock__"):
+            unsafe = object.__getattribute__(self, "__unsafe__")
+            if item in unsafe:
+                value = unsafe[item]
+                object.__setattr__(self, item, value)
+                return value
+
+        try:
+            return object.__getattribute__(self, item)
+        except AttributeError as e:
+            if config.DEBUG:
+                print("bucket not filled")
+                raise e
+            else:
+                if not item.startswith("_"):
+                    print("FAIL: ", item)
+                return None
+
+    def __contains__(self, item):
+        try:
+            self[item]
+            return True
+        except NameError:
+            return False
+
+    def __getitem__(self, item):
+        """Returns item in the bucket or if none is found, the item in the unsafe part"""
+        prevlock = self.__lock__
+        + self
+        try:
+            return self.__getattribute__(item)
+        except AttributeError:
+            return self.__unsafe__[item]
+        finally:
+            self.__lock__ = prevlock
+
+    def __setitem__(self, item, value):
+        prevlock = self.__lock__
+        + self
+        try:
+            if hasattr(self, item): #erroneoussssss
+                self.__setattr__(item, value)
+            else:
+                raise AttributeError("To prevent sqlinjections you cant declare new attributes like this")
+        finally:
+            self.__lock__ = prevlock
+
+    def __pos__(self):
+        self.__lock__ = True
+
+    def __neg__(self):
+        self.__lock__ = False
+
+
+    def __iter__(self):
+        return (x for x in dir(self) if not x.startswith("_"))
+
+    def __rshift__(self, args):
+        """Pour, into database"""
+        sql = args[0]
+        args = args[1:]
+
+        setstatm = ", ".join(["{0} = ?".format(c) for c in self])
+        if sql.lower().find(" set ") == -1:
+            sql = sql.replace("$", "SET $")
+        sql = sql.replace("$", setstatm)
+
+        values = [self[c] for c in self]
+        values.extend(args)
+
+        return execute(sql, *values)
+
+    def __ge__(self, dest):
+        """Create entry in database"""
+        sql = "INSERT INTO {0}(".format(dest)
+        keys = [c for c in self]
+        questions = ["?"] * len(keys)
+        values = [self[c] for c in self]
+
+        sql += ",".join(keys)
+        sql += ") VALUES ("
+        sql += ",".join(questions)
+        sql += ")"
+        sql += " returning *"
+
+        result = execute(sql, *values)
+        if len(result) == 1:
+            return result[0]
+        return result
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python data.py SCRIPT")
+    else:
+        print(script(sys.argv[1]))
