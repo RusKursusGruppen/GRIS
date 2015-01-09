1de211cf49d5f2df2b2facd8e0ad5dea645d6f98
Pilen <fiskomaten@gmail.com>
2015-01-10 00:06:09 +0100

    added sleep function to stall "attackers"

--- lib/tools.py
+++ lib/tools.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import datetime
+import datetime, random, time
 import werkzeug
 
 def abort(code=None, description=None):
@@ -17,3 +17,13 @@ def abort(code=None, description=None):
 
 def now():
     return datetime.datetime.now(datetime.timezone.utc)
+
+def sleep(start, end=None):
+    if end is not None:
+        time.sleep(start, end)
+    elif isinstance(start, (tuple, list)):
+        a, b = start
+        t = a + random.random() * (b - a)
+        time.sleep(t)
+    else:
+        time.sleep(start)
