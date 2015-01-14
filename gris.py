77754abc82a653e8c37ef70a25397a0a47aebd4d
Pilen <fiskomaten@gmail.com>
2015-01-14 13:52:19 +0100

    added updated gris.py

--- gris.py
+++ gris.py
@@ -2,23 +2,27 @@
 
 import atexit
 
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_mail import Mail
 import flask_mail
 
+from lib.data import BucketDatabase
 import config
 
 app = Flask(__name__)
 app.config.from_object("config")
 
+data = BucketDatabase(app)
 mail = Mail(app)
 
-# import models
+from server.usermanager import blueprint as usermanager_blueprint
+app.register_blueprint(usermanager_blueprint)
 
-# import server.usermanagement
 
-@app.route("/")
-def index():
+@app.route("/", defaults={"path":""})
+@app.route("/<path:path>")
+def index(path):
+    print(app.url_map)
     return "Hello, World!"
 
 if __name__ == "__main__":
