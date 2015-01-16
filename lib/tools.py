d5d8f5c45a7ca44758a41411469c7163668ced8f
Pilen <fiskomaten@gmail.com>
2015-01-16 23:17:06 +0100

    added logged_in, and is_admin

--- lib/tools.py
+++ lib/tools.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import datetime, random, time
+from functools import wraps
 import werkzeug
+from flask import request, session
 
 def abort(code=None, description=None):
     _abort = werkzeug.exceptions.abort
@@ -27,3 +29,48 @@ def sleep(start, end=None):
         time.sleep(t)
     else:
         time.sleep(start)
+
+
+def logged_in(*args):
+    # EXPLANATION: logged_in is called as a decorator
+    if callable(args[0]):
+        fn = args[0]
+        @wraps(fn)
+        def decorated(*args, **kwargs):
+            if not session.get("logged_in"):
+                session["login_origin"] = request.path
+                abort(403)
+            else:
+                return fn(*args, **kwargs)
+        return decorated
+
+    else:
+        # EXPLANATION: decorator factory called with something to iterate
+        if isinstance(args[0], str):
+            rights = list(args)
+        else:
+            rights = list(args[0])
+
+        # Admins can do everything
+        rights.append("admin")
+
+        def decorator(fn):
+            @wraps(fn)
+            def decorated(*args, **kwargs):
+                if not session.get("logged_in"):
+                    session["login_origin"] = request.path
+                    abort(403)
+                else:
+                    groups = lib.data.execute("SELECT groupname FROM Group_users WHERE user_id = ?", session["user_id"])
+                    for group in groups:
+                        if group["groupname"] in rights:
+                            return fn(*args, **kwargs)
+                    abort(403)
+            return decorated
+        return decorator
+
+def is_admin():
+    if "is_admin" not in flask.g:
+        groups = lib.data.execute("SELECT groupname FROM Group_users WHERE user_id = ?", session["user_id"]).scalars()
+        flask.g.is_admin = "admin" in groups
+    return flask.g.is_admin
