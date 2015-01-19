90947c43417f5616feefed003689efb096c2e8c5
Pilen <fiskomaten@gmail.com>
2015-01-19 22:41:20 +0100

    added gris with imports

--- gris.py
+++ gris.py
@@ -2,7 +2,7 @@
 
 import atexit
 
-from flask import Flask, Blueprint, session
+from flask import Flask, Blueprint, send_file, session
 from flask_mail import Mail
 import flask_mail
 
@@ -15,17 +15,32 @@ app.config.from_object("config")
 data = BucketDatabase(app)
 mail = Mail(app)
 
-from server.usermanager import blueprint as usermanager_blueprint
-app.register_blueprint(usermanager_blueprint)
+from server.usermanager import blueprint
+app.register_blueprint(blueprint)
 
-from server.news import blueprint as news_blueprint
-app.register_blueprint(news_blueprint)
+from server.news import blueprint
+app.register_blueprint(blueprint)
+
+from server.rustours import blueprint
+app.register_blueprint(blueprint)
+
+from server.mentorteams import blueprint
+app.register_blueprint(blueprint)
+
+from server.rusmanager import blueprint
+app.register_blueprint(blueprint)
 
 
 @app.route("/", defaults={"path":""})
-@app.route("/<path:path>")
+# @app.route("/<path:path>")
 def index(path):
-    return "Hello, World!"
+    return send_file("static/html/index.html")
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    b = data.request()
+    print(b)
+    return "yay"
 
 if __name__ == "__main__":
     app.run(config.HOST, config.PORT)
