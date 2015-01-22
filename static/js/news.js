3a1f90472dfac2cea4e2a4e94c8a27b3ef744103
Pilen <fiskomaten@gmail.com>
2015-01-22 05:09:27 +0100

    news are comming along

--- static/js/news.js
+++ static/js/news.js
@@ -1,7 +1,28 @@
 
 var news = angular.module("news", []);
 
-news.controller("newsCtrl", function($scope) {
-    $scope.news = [{title:"hi", body:"Hello, World!", created:"12:49", creator:"23141"}, {title:"abc", body:"def", created:"12:49", creator:"23141"}];
-    $scope.name = "Hello"
+news.controller("newsCtrl", function($scope, $http) {
+    $http.get("/api/news").success(function(data) {
+        $scope.news = data.values;
+    });
+
+    $scope.test = false;
+
+    $scope.create_new = function(form) {
+        if (!form.$invalid) {
+            $http.post("/api/news/new", {title:$scope.title, body:$scope.body});
+            $scope.cancel(form);
+        }
+    };
+
+    $scope.cancel = function(form) {
+        $scope.title = "";
+        $scope.body = "";
+        if (form) {
+            form.$setPristine();
+            form.$setUntouched();
+        }
+        $scope.creating_mode = false;
+    };
+    $scope.cancel();
 });
