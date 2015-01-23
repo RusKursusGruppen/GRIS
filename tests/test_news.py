6d1e37cd1b1eb965b4b53e90f0d1f89a2ab8f120
Pilen <fiskomaten@gmail.com>
2015-01-23 19:50:28 +0100

    renamed login stuff in tests

--- tests/test_news.py
+++ tests/test_news.py
@@ -12,22 +12,22 @@ class TestNews(ApplicationTestBase):
         self.assertEqual(result, {"values":[], "length":0})
 
     def test_02_new_missing_title(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         result = self.post("/api/news/new", data={"title":"", "body":"Hello"})
         self.assertAborted(400, "illegal title")
 
     def test_03_new_missing_body(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         result = self.post("/api/news/new", data={"title":"Hello", "body":""})
         self.assertAborted(400, "illegal body")
 
     def test_04_new(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         result = self.post("/api/news/new", data={"title":"Hello, World!", "body":"Welcome to GRIS!"})
         self.assertSuccess()
 
     def test_05_new(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         result = self.post("/api/news/new", data={"title":"Second message", "body":"With a body"})
         self.assertSuccess()
 
@@ -47,7 +47,7 @@ class TestNews(ApplicationTestBase):
         self.assertEqual(result["values"][1]["creator"]["user_id"], 1)
 
     def test_07_update(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         result = self.post("/api/news/update", data={"news_id":2, "title":"Second message!", "body":"With a body!"})
         self.assertSuccess()
 
@@ -60,7 +60,7 @@ class TestNews(ApplicationTestBase):
         self.assertEqual(result["values"][1]["creator"]["user_id"], 1)
 
     def test_08_update(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         result = self.post("/api/news/update", data={"news_id":2, "body":"With a body"})
         self.assertSuccess()
 
@@ -73,7 +73,7 @@ class TestNews(ApplicationTestBase):
         self.assertEqual(result["values"][1]["creator"]["user_id"], 1)
 
     def test_09_delete(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         result = self.post("/api/news/new", data={"title":"third", "body":"content"})
         self.assertSuccess()
 
@@ -96,21 +96,21 @@ class TestNews(ApplicationTestBase):
 
     #TODO: test update and delete as wrong user
     def test_10_update_as_wrong_user(self):
-        self.post("/api/usermanager/authenticate", data={"username":"fugl", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"fugl", "raw_password":"123"})
         result = self.post("/api/news/update", data={"news_id":2, "title":"Second message!", "body":"With a body!"})
         self.assertAborted(403, "You are not the creator")
 
     def test_11_delete_as_wrong_user(self):
-        self.post("/api/usermanager/authenticate", data={"username":"fugl", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"fugl", "raw_password":"123"})
         result = self.post("/api/news/delete", data={"news_id":2})
         self.assertAborted(403, "You are not the creator")
 
     def test_12_update_missing_title(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         result = self.post("/api/news/update", data={"news_id":2, "title":"", "body":"Hello"})
         self.assertAborted(400, "illegal title")
 
     def test_13_update_missing_body(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         result = self.post("/api/news/update", data={"news_id":2, "title":"Hello", "body":""})
         self.assertAborted(400, "illegal body")
--- tests/test_usermanager.py
+++ tests/test_usermanager.py
@@ -10,62 +10,62 @@ from server import usermanager
 from gris import app
 
 class TestAuthentication(ApplicationTestBase):
-    def test_01_authenticate_1(self):
-        result = self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+    def test_01_login_1(self):
+        result = self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         self.assertSuccess()
 
-    def test_02_authenticate_2(self):
+    def test_02_login_2(self):
         with self.app:
-            result = self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+            result = self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
             self.assertSuccess()
             self.assertEqual(session["logged_in"], True)
             self.assertEqual(session["user_id"], 1)
             self.assertEqual(session["username"], "rkg")
 
-    def test_03_unauthenticate_1(self):
+    def test_03_logout_1(self):
         with self.app:
-            result = self.post("/api/usermanager/unauthenticate")
+            result = self.post("/api/usermanager/logout")
             self.assertSuccess()
             self.assertFalse(hasattr(session, "logged_in"))
             self.assertFalse(hasattr(session, "user_id"))
             self.assertFalse(hasattr(session, "username"))
 
-    def test_04_unauthenticate_2(self):
-        "unauthenticate while not authenticated"
+    def test_04_logout_2(self):
+        "logout while not logged_in"
         with self.app:
-            result = self.post("/api/usermanager/unauthenticate")
+            result = self.post("/api/usermanager/logout")
             self.assertSuccess()
             self.assertFalse(hasattr(session, "logged_in"))
             self.assertFalse(hasattr(session, "user_id"))
             self.assertFalse(hasattr(session, "username"))
 
-    def test_05_is_not_authenticated(self):
-        result = self.post_json("/api/usermanager/authenticated")
+    def test_05_is_not_logged_in(self):
+        result = self.post_json("/api/usermanager/logged_in")
         self.assertSuccessful()
-        self.assertEqual(result["authenticated"], False)
+        self.assertEqual(result["logged_in"], False)
 
-    def test_06_is_authenticated(self):
-        self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
+    def test_06_is_logged_in(self):
+        self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
         self.assertSuccess()
-        result = self.post_json("/api/usermanager/authenticated")
+        result = self.post_json("/api/usermanager/logged_in")
         self.assertSuccessful()
-        self.assertEqual(result["authenticated"], True)
+        self.assertEqual(result["logged_in"], True)
 
-    def test_07_is_authenticated_session(self):
+    def test_07_is_logged_in_session(self):
         with self.app:
-            self.post("/api/usermanager/authenticate", data={"username":"rkg", "raw_password":"123"})
-            result = self.post_json("/api/usermanager/authenticated")
+            self.post("/api/usermanager/login", data={"username":"rkg", "raw_password":"123"})
+            result = self.post_json("/api/usermanager/logged_in")
             self.assertSuccessful()
-            self.assertEqual(result["authenticated"], True)
+            self.assertEqual(result["logged_in"], True)
             self.assertEqual(session["logged_in"], True)
             self.assertEqual(session["user_id"], 1)
             self.assertEqual(session["username"], "rkg")
 
-    def test_08_each_case_will_start_unauthenticate(self):
+    def test_08_each_case_will_start_logout(self):
         with self.app:
-            result = self.post_json("/api/usermanager/authenticated")
+            result = self.post_json("/api/usermanager/logged_in")
             self.assertSuccessful()
-            self.assertEqual(result["authenticated"], False)
+            self.assertEqual(result["logged_in"], False)
             self.assertFalse(hasattr(session, "logged_in"))
             self.assertFalse(hasattr(session, "user_id"))
             self.assertFalse(hasattr(session, "username"))
