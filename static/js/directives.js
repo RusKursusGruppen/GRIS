041e57a0fc62669e50d8175aee9efe9631447a86
Pilen <fiskomaten@gmail.com>
2015-02-03 10:01:02 +0100

    added some directives, removed some filters

--- static/js/directives.js
+++ static/js/directives.js
@@ -14,3 +14,24 @@ gris.directive("isActiveNav", function($location) {
         }
     };
 });
+
+gris.directive("gUser", function($location) {
+    return {
+        scope: { user: "=user" },
+        template: "<a href='' class='user' tooltip='{{user.name}}'><i class='fa fa-user'></i> {{user.username}}</a>"
+    };
+});
+
+gris.directive("gRustourType", function() {
+    return {
+        scope: { tour: "=tour" },
+        template:
+        "<span ng-switch='tour.type'>" +
+            "<span ng-switch-when='m' tooltip='Munketur'><i class='fa fa-mars'></i></span>" +
+            "<span ng-switch-when='p' tooltip='Pigetur'><i class='fa fa-venus'></i></span>" +
+            "<span ng-switch-when='t' tooltip='Transetur'><i class='fa fa-transgender'></i></span>" +
+            "<span ng-switch-default tooltip='Uvist type'><i class='fa fa-circle-thin'></i></span>" +
+        "</span>"
+
+    };
+});
